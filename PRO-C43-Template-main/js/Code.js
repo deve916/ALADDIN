b89ea6d461a9a335e69dbf1ee1b529c0e56851ea
deve916 <83933482+deve916@users.noreply.github.com>
2021-07-17 18:37:18 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text(" ATADASBA", 700,150)
+    fill("lightblue")
+    text("Hint: STORES INFORMATION !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("CUTNIFON.", 100,250)
+    fill("lightblue")
+    text("Hint: STORES INFORMATION !!", 100,270)
+
+    
+
+}
